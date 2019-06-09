71c162fcc9065abd29d2375dda106956fc21ae58
Your Name <sbernal2018@gmail.com>
2019-06-09 12:20:11 -0400

    latest

---
+++ startbootstrap-sb-admin-2-gh-pages/core.js
@@ -0,0 +1,37 @@
+
+
+
+<html lang="en">
+  <head>
+    <title>Agora Group Video Chat Demo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <div id="container">
+      <div id="main-container">
+        <div id="screen-share-btn-container">
+          <!-- insert button to share screen -->
+        </div>
+        <div id="buttons-container">
+          <!-- insert buttons to toggle audio/video and leave/end call -->
+        </div>
+        <div id="full-screen-video"></div>
+        <div id="lower-video-bar">
+          <div id="remote-streams-container">
+            <div id="remote-streams">
+              <!-- insert remote streams dynamically -->
+            </div>
+          </div>
+          <div id="local-stream-container">						
+            <div id="local-video"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <!-- CSS Includes go here -->
+  <!-- JS Includes go here -->
+</html>
+
+
